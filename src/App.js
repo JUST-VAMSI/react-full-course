dad33e52b2f129147b5273c5ca58c3c9ea2ba577
JUST-VAMSI <veeramallavamsi72@gmail.com>
2023-06-19 14:46:48 +0530

    react router dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from "react";
-import Final from "./8-custome hook/FirstAPI";
-import Final2 from "./8-custome hook/SecondAPI"
+// import Final from "./8-custome hook/FirstAPI";
+// import Final2 from "./8-custome hook/SecondAPI";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import {Routes,Route} from "react-router-dom";
+
 
 
 function App() {
 
   return (
     <div>
-   <Final/>
-   <Final2/>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
+        <Route path="/contact" element={<Contact/>}></Route>
+      </Routes>
    </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { UserContextProvider } from './context/userContext';
+import { BrowserRouter } from 'react-router-dom';
+// import { UserContextProvider } from './context/userContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <UserContextProvider>
+  <BrowserRouter>
     <App/>
-  </UserContextProvider>
+    </BrowserRouter>
+ 
 );
